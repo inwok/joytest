2039cee6e9fe0a64a3ccf861d4351c1bda7c6a17
inwo <145432455+inwok@users.noreply.github.com>
2023-11-09 17:00:59 +0700

    Add files via upload

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ namespace joy {
     
     //% block="P11 pressed"
     export function onButtonPressed(body: () => void) {
-        pins.setPull(DigitalPin.P11, PinPullMode.Up); 
+        //pins.setPull(DigitalPin.P11, PinPullMode.Up); 
         if (pins.digitalReadPin(DigitalPin.P11) == 0) {
             body();
         }

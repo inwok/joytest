4f2c340cfaa0d497686e7a8c47053add6d6b2c45
inwo <145432455+inwok@users.noreply.github.com>
2023-11-09 16:55:34 +0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+//% color="#AA278D"
+namespace joy {
+    
+    //% block="P11 pressed"
+    export function onButtonPressed(body: () => void) {
+        pins.setPull(DigitalPin.P11, PinPullMode.Up); 
+        if (pins.digitalReadPin(DigitalPin.P11) == 0) {
+            body();
+        }
+    }
+}
